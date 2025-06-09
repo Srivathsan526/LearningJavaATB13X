917067528ba18f5304a08dfe83552126f02bf922
Srivathsan 526 <srivathsan.510@gmail.com>
2025-06-09 18:54:34 +1200

    Task

---
+++ src/Day01_5thJune2025/Lab006_MultiComments.java
@@ -0,0 +1,12 @@
+package Day01_5thJune2025;
+
+public class Lab006_MultiComments {
+    public static void main(String[] args) {
+        /*
+         * Author : Pramod Dutta
+         * Course :  ATB13x
+         *
+         */
+        System.out.println("Multi Comment");
+    }
+}
