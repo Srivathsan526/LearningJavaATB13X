855efdead623685cf6b18a3982b50fd79ff769b6
Srivathsan 526 <srivathsan.510@gmail.com>
2025-06-09 18:57:06 +1200

    Task

---
+++ src/Day01_5thJune2025/Lab008_Comments.java
@@ -0,0 +1,14 @@
+package Day01_5thJune2025;
+
+public class Lab008_Comments {
+    /**
+     * Author - Pramod
+     * Course :  ATB13x
+     * This is a documentation comment
+     * @param args command-line arguments
+     * @return void
+     */
+    public static void main(String[] args) {
+        //Method Implementation
+    }
+}
