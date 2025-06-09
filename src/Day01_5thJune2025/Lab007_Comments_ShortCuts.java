5b96ab98c57f29e7576b3f596c9618a2e8d2a070
Srivathsan 526 <srivathsan.510@gmail.com>
2025-06-09 18:55:45 +1200

    Task

---
+++ src/Day01_5thJune2025/Lab007_Comments_ShortCuts.java
@@ -0,0 +1,9 @@
+package Day01_5thJune2025;
+
+public class Lab007_Comments_ShortCuts {
+    // This code will print the Hi
+    // Jr QA - this will print the hi in console
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
