2231a8611e5cd70b0481a58486db94b7f23cab1e
Srivathsan 526 <srivathsan.510@gmail.com>
2025-06-11 19:36:39 +1200

    Task

---
+++ src/June10/Lab001.java
@@ -0,0 +1,10 @@
+package June10;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("Hello2");
+        System.out.print("Hello3");
+        System.out.println("Hello4");
+    }
+}
---
+++ src/June10/Table9.java
@@ -0,0 +1,16 @@
+package June10;
+
+public class Table9 {
+    public static void main(String[] args) {
+        System.out.printf("1 * 9 = %d%n", 1 * 9);
+        System.out.printf("2 * 9 = %d%n", 2 * 9);
+        System.out.printf("3 * 9 = %d%n", 3 * 9);
+        System.out.printf("4 * 9 = %d%n", 4 * 9);
+        System.out.printf("5 * 9 = %d%n", 5 * 9);
+        System.out.printf("6 * 9 = %d%n", 6 * 9);
+        System.out.printf("7 * 9 = %d%n", 7 * 9);
+        System.out.printf("8 * 9 = %d%n", 8 * 9);
+        System.out.printf("9 * 9 = %d%n", 9 * 9);
+        System.out.printf("10 * 10 = %d%n", 10 * 9);
+    }
+}
