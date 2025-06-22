bb10b44591913cf1e13d8da7d30bc0b8a69b0123
Srivathsan 526 <srivathsan.510@gmail.com>
2025-06-23 07:27:04 +1200

    Task

---
+++ src/June19/Challenge2.java
@@ -0,0 +1,27 @@
+package June19;
+
+public class Challenge2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        a += b;
+        System.out.println("Addition: " + a );
+        int c = 10;
+        int d = 5;
+        c -= d;
+        System.out.println("Subtraction: " + c );
+        int e = 10;
+        int f = 5;
+        e *= f;
+        System.out.println("Multiplication: " + e );
+        int g = 10;
+        int h = 5;
+        g /= h;
+        System.out.println("Division: " + g );
+        int i = 10;
+        int j = 5;
+        i %= j;
+        System.out.println("Modulus: " + i );
+
+    }
+}
