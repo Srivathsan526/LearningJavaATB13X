3bf346351e9b64f3d23d06706ca0e2299b954092
Srivathsan 526 <srivathsan.510@gmail.com>
2025-06-23 07:45:45 +1200

    Task

---
+++ src/June19/Challenge4.java
@@ -0,0 +1,13 @@
+package June19;
+
+public class Challenge4 {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("Original: " + i + " (returned i = 5)");
+        System.out.println("Pre-Increment: " + ++i + " (returned i = 6)");
+        System.out.println("Post-Increment: " + i++ + " (returned i = 7)");
+        System.out.println("Pre-Decrement: " + --i + " (returned i = 6)");
+        System.out.println("Post-Decrement: " + i-- + " (returned i = 5)");
+
+    }
+}
