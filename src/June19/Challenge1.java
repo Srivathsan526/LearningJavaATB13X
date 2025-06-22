1a402b7531db1696fe1ab1249c66c391cef3e4d8
Srivathsan 526 <srivathsan.510@gmail.com>
2025-06-23 07:12:23 +1200

    Task

---
+++ src/June19/Challenge1.java
@@ -0,0 +1,22 @@
+package June19;
+
+public class Challenge1 {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println("Integer: " + a);
+        char b = 'B';
+        System.out.println("Character: " + b);
+        float c = 3.14f;
+        System.out.println("Float: " + c);
+        double d = 8.26;
+        System.out.println("Double: " + d);
+        byte e = 2;
+        System.out.println("Byte: " + e);
+        long f = 3456L;
+        System.out.println("Long: " + f);
+        boolean g = true;
+        System.out.println("Boolean: " + true);
+        short h = 3;
+        System.out.println("Short: " + h);
+    }
+}
