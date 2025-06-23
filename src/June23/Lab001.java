df77565a2227ba8bfcbfa15dbc680e48f0c26b72
Srivathsan 526 <srivathsan.510@gmail.com>
2025-06-23 14:38:39 +1200

    Task

---
+++ src/June23/Lab001.java
@@ -0,0 +1,9 @@
+package June23;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        String s1 = "Sonal@123@live.com";
+        String[] s2 = s1.split("@");
+        System.out.println(s2[0]);
+    }
+}
